7604c2c3b966d221426e7baf532a2259c43bd154
Molly McCarron <mccarronmollye@gmail.com>
2019-02-16 15:13:16 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def home
+  end 
+
+
+  protect_from_forgery with: :exception
+end
---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,18 @@
+class StoreAdminController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+  def home
+    render :layout => "admin"
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
+  end
+
+  protect_from_forgery with: :exception
+end
